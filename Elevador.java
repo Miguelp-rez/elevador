e6384498ff02d8c59303be8f2601853dae79ca29
Miguel Pérez <miguel@MacBook-Air-de-Miguel.local>
2018-03-25 13:36:06 -0600

    Se corrigen errores en clase Elevador

--- Elevador.java
+++ Elevador.java
@@ -8,26 +8,33 @@ public class Elevador{
     }
 
     public void subePiso(int destino){  //Recibe el piso destino
-        for(int i = pisoActual; i <= destino; i++){
+        for(int i = this.pisoActual + 1; i <= destino; i++){
             System.out.println("Subiendo al piso " + i);
         }
+        System.out.println("Ha llegado al piso " + destino);
         pisoActual = destino;
     }
+
     public void bajaPiso(int destino){  //Recibe el piso destino
-        for(int i = pisoActual; i >= destino; i--){
+        for(int i = this.pisoActual - 1 ; i >= destino; i--){
             System.out.println("Bajando al piso " + i);
         }
+        System.out.println("Ha llegado al piso " + destino);
         pisoActual = destino;
     }
-    public void subePersona(){
+
+    public void subePersona(){  //Sube una persona
         this.numPersonas++;
     }
-    public void bajaPersona(){
+
+    public void bajaPersona(){  //Baja una persona
         this.numPersonas--;
     }
+
     public int getNumPersonas(){
         return this.numPersonas;
     }
+
     public int getPiso(){
         return this.pisoActual;
     }
